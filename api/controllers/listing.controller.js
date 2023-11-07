ed009b56f47a3f8252527e9140aab421d517d5aa
SamyoPramanik <2105167@ugrad.cse.buet.ac.bd>
2023-11-07 22:46:51 +0600

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -58,3 +58,49 @@ export const getListing = async (req, res, next) => {
         next(errorHandler(500, err.message));
     }
 };
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const start = parseInt(req.query.start) || 0;
+
+        let offer = req.query.offer;
+        if (offer === "false" || offer === undefined) {
+            offer = { $in: [true, false] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === "false" || parking === undefined) {
+            parking = { $in: [true, false] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === "false" || furnished === undefined) {
+            furnished = { $in: [true, false] };
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === "all") {
+            type = { $in: ["sale", "rent"] };
+        }
+
+        const searchTerm = req.query.searchTerm || "";
+        const sort = req.query.sort || "createdAt";
+        const order = req.query.order || "desc";
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: "i" },
+            offer,
+            parking,
+            furnished,
+            type,
+        })
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(start);
+
+        res.status(200).json(listings);
+    } catch (err) {
+        next(errorHandler(500, err.message));
+    }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
     deleteListing,
     getListing,
     updateListing,
+    getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -14,5 +15,6 @@ listingRoute.post("/create", verifyToken, createListing);
 listingRoute.delete("/delete/:id", verifyToken, deleteListing);
 listingRoute.post("/update/:id", verifyToken, updateListing);
 listingRoute.get("/get/:id", getListing);
+listingRoute.get("/get", getListings);
 
 export default listingRoute;

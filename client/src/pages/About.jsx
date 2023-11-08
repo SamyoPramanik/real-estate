511cf7fd5793bcb879bb25bb8a5d212f69404cf2
SamyoPramanik <2105167@ugrad.cse.buet.ac.bd>
2023-11-09 01:38:22 +0600

    updated about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,21 +7,13 @@ export default function About() {
                 About Real World
             </h1>
             <p className="mb-4 text-slate-800">
-                Real World is a real estate that in helpmg clients buy. sell.
-                and rent in the most desirable ne•mrhotxis. Our team ot iS to
-                exceptional rnaking the and as Ow mission is to help our clients
-                achieve their goals by providing exg»rt Mvice. service. and a
-                deep of the local market.
+                An website develped by Samyo Pramanik
             </p>
             <p className="mb-4 text-slate-800">
-                you are mking to buy, sell, or rent a property, we are here to
-                help you every step ot the way.
+                An website develped by Samyo Pramanik
             </p>
             <p className="mb-4 text-slate-800">
-                tean Of agents has a Of in real estate We are to the Of Ce to
-                clients. We that buying a an exciting and rewarding we dedicated
-                to rnakfrlg that a reality for each and every one of our
-                clients.
+                An website develped by Samyo Pramanik
             </p>
         </div>
     );

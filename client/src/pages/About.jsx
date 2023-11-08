2d464b925519bf9685fb309482a56a252ca66065
SamyoPramanik <2105167@ugrad.cse.buet.ac.bd>
2023-11-08 20:55:20 +0600

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">
+                About Real World
+            </h1>
+            <p className="mb-4 text-slate-800">
+                Real World is a real estate that in helpmg clients buy. sell.
+                and rent in the most desirable ne•mrhotxis. Our team ot iS to
+                exceptional rnaking the and as Ow mission is to help our clients
+                achieve their goals by providing exg»rt Mvice. service. and a
+                deep of the local market.
+            </p>
+            <p className="mb-4 text-slate-800">
+                you are mking to buy, sell, or rent a property, we are here to
+                help you every step ot the way.
+            </p>
+            <p className="mb-4 text-slate-800">
+                tean Of agents has a Of in real estate We are to the Of Ce to
+                clients. We that buying a an exciting and rewarding we dedicated
+                to rnakfrlg that a reality for each and every one of our
+                clients.
+            </p>
+        </div>
+    );
 }
